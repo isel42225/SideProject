86a48eb46bc081184ab2a25e950b0ad222bcd8ee
MiguelMMatos <miguelmatosfac@gmail.com>
2019-04-09 21:28:58 +0100

    Inicio

---
+++ Jogo2D/Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    float runSpeed = 40f;
+    float horizontalMove = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    }
+     void FixedUpdate()
+    {
+        controller.Move(horizontalMove*Time.fixedDeltaTime,false,false);
+        
+    }
+}
