ee09fc38b45c83f4c02384f9f8c4ab7bb413586a
MiguelMMatos <miguelmatosfac@gmail.com>
2019-04-10 13:00:37 +0100

    Atualizacao
    Implementação do jump e do crouch ( Crouch tem um bug que o boneco fico sempre a dar crouch e levantar ao mesmo tempo).

--- Jogo2D/Assets/PlayerMovement.cs
+++ Jogo2D/Assets/PlayerMovement.cs
@@ -6,17 +6,33 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
 
-    float runSpeed = 40f;
+    float runSpeed = 60f;
     float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        if (Input.GetButtonDown("Crouch")){
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
     }
      void FixedUpdate()
     {
-        controller.Move(horizontalMove*Time.fixedDeltaTime,false,false);
+        controller.Move(horizontalMove*Time.fixedDeltaTime, crouch, jump);
+        Debug.Log(crouch);
+        jump = false;
         
     }
 }
