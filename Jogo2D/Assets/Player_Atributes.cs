9e9bbec41d3c8616d24ef3ed6e0bef92b7853fc3
MiguelMMatos <miguelmatosfac@gmail.com>
2019-04-12 15:14:30 +0100

    Mais Melhorias no AI

---
+++ Jogo2D/Assets/Player_Atributes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Atributes : MonoBehaviour
+{
+    public Transform ReachEndOfPath;
+    public int Vida=100;
+
+    // Update is called once per frame
+    int FixedUpdate()
+    {
+        if (ReachEndOfPath==true)
+        {
+            Vida = 0;
+            return Vida;
+        }
+        return Vida;
+    }
+}
